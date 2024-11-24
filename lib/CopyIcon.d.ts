2af90aa10473ce4c82fc2c0e311a54084fa2516d
yjgaia <yjdev88@gmail.com>
2024-11-24 22:11:29 +0900

    Add CloseIcon and CopyIcon components, update exports in index files

---
+++ lib/CopyIcon.d.ts
@@ -0,0 +1,5 @@
+import { SvgIcon } from "@common-module/app-components";
+export default class CopyIcon extends SvgIcon {
+    constructor();
+}
+//# sourceMappingURL=CopyIcon.d.ts.map
---
+++ src/CloseIcon.ts
@@ -0,0 +1,11 @@
+import { SvgIcon } from "@common-module/app-components";
+
+export default class CloseIcon extends SvgIcon {
+  constructor() {
+    super(
+      ".close",
+      "0 -960 960 960",
+      '<path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>',
+    );
+  }
+}
