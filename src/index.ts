0f59f3a65473d2b1ac2b0518e498f1e9788121c4
yjgaia <yjdev88@gmail.com>
2024-12-22 13:48:15 +0900

    Add SettingsIcon and WarningIcon components; update exports in index files

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,9 @@ export { default as ProfileIcon } from "./ProfileIcon.js";
 export { default as RefreshIcon } from "./RefreshIcon.js";
 export { default as SearchIcon } from "./SearchIcon.js";
 export { default as SendIcon } from "./SendIcon.js";
+export { default as SettingsIcon } from "./SettingsIcon.js";
 export { default as SuccessIcon } from "./SuccessIcon.js";
 export { default as UploadIcon } from "./UploadIcon.js";
 export { default as WalletIcon } from "./WalletIcon.js";
 export { default as WarningIcon } from "./WarningIcon.js";
 export { default as XIcon } from "./XIcon.js";
-
